37b81d5e44e6432fdf648565b53ca7663d912ed6
Oliver Charles <oliver.g.charles@googlemail.com>
2011-06-01 12:23:51 +0100

    Merge RAWDATA into READWRITE in the musicbrainz_raw schema

--- upgrade.sh
+++ upgrade.sh
@@ -5,24 +5,28 @@ cd `dirname $0`
 
 eval `./admin/ShowDBDefs`
 
-echo `date` : Backing up data
-./admin/psql READWRITE < ./admin/sql/updates/20110525-READWRITE-backup.sql
-./admin/psql RAWDATA < ./admin/sql/updates/20110525-RAWDATA-backup.sql
+echo `date` : Renaming RAWDATA musicbrainz schema
+echo 'ALTER SCHEMA musicbrainz RENAME TO musicbrainz_raw' | ./admin/psql RAWDATA
+echo 'CREATE SCHEMA musicbrainz_raw' | ./admin/psql READWRITE
 
-echo `date` : Fixing broken time zones
-./admin/psql READWRITE < ./admin/sql/updates/20110525-invalid-timezones.sql
+echo `date` : Dumping RAWDATA
+eval $(perl -Ilib -MString::ShellQuote -MDBDefs -MMusicBrainz::Server::DatabaseConnectionFactory -e '
+    my $db = MusicBrainz::Server::DatabaseConnectionFactory->get("RAWDATA");
+    my $rw = MusicBrainz::Server::DatabaseConnectionFactory->get("READWRITE");
+    printf "export MB_RAWDATA_%s=%s\n", uc($_), shell_quote($db->$_),
+        for qw( username password schema port database host );
+    printf "export MB_READWRITE_%s=%s\n", uc($_), shell_quote($rw->$_),
+        for qw( username password schema port database host );
+')
+pg_dump --format=c --schema=musicbrainz_raw --host=$MB_RAWDATA_HOST --username=$MB_RAWDATA_USERNAME -v \
+    $MB_RAWDATA_DATABASE > rawdata.dump
 
-echo `date` : Rewriting short link phrases for relationships
-./admin/psql READWRITE < ./admin/sql/updates/20110524-short-link-phrases.sql
+echo `date` : Loading RAWDATA into READWRITE
+pg_restore --format=c --schema=musicbrainz_raw --host=$MB_READWRITE_HOST --username=$MB_READWRITE_USERNAME -v \
+    -d $MB_READWRITE_DATABASE rawdata.dump
 
-echo `date` : Upgrading relationship edit types to include short link phrases
-./admin/sql/updates/20110524-short-link-phrase-edits.pl
-
-echo `date` : Relinking relationship edits against artists
-./admin/sql/updates/20110524-relink-relationships.pl
-
-echo `date` : Fixing edit relationship edits
-./admin/sql/updates/20110524-fix-broken-relationship-edits.pl
+echo `date` : RAWDATA is now part of the READWRITE database.
+echo `date` : Please update your DBDefs and feel free to drop the RAWDATA database
 
 echo `date` : Done
 
